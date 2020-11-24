5a3b2ecc989eed1d010e2a0516aaf55de17e0258
samuel zaťovič <szatovic@gmail.com>
2020-11-24 13:59:08 +0100

    Rozsirenie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { ItemsComponent } from './items/items.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    ItemsComponent
+    HeroDetailComponent,
+    ItemsComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -1,14 +1,14 @@
 import { Item } from './Item';
 
 export const ITEMS: Item[] = [
-  { id: 2, price: 2, name: 'Banana' },
-  { id: 3, price: 3, name: 'Orange' },
-  { id: 4, price: 5, name: 'broccoli' },
-  { id: 5, price: 1, name: 'Spinach' },
-  { id: 6, price: 2, name: 'Peach' },
-  { id: 7, price: 10, name: 'Watermelon' },
-  { id: 8, price: 1, name: 'Garlic' },
-  { id: 9, price: 2, name: 'Onion' },
-  { id: 10, price: 2, name: 'Lemon' },
-  { id: 11, price: 3, name: 'Bluberry' }
+  { id: 2, price: 1, name: 'Cloth armor' },
+  { id: 3, price: 3, name: 'Iron armor' },
+  { id: 4, price: 5, name: 'Steel armor' },
+  { id: 5, price: 1, name: 'Wooden sword' },
+  { id: 6, price: 4, name: 'Iron sword' },
+  { id: 7, price: 7, name: 'Steel sword' },
+  { id: 8, price: 2, name: 'Bicycle helmet' },
+  { id: 9, price: 4, name: 'Iron helmet' },
+  { id: 10, price: 6, name: 'Steel helmet' },
+  { id: 11, price: 10, name: 'Ultimate helmet' }
 ];
