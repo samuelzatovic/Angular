cd3c4d511d5534b8d9cf2e39f43b81ba335d4c7d
samuel zaťovič <szatovic@gmail.com>
2020-11-24 18:46:22 +0100

    Prepojenie

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,7 @@
+import { from } from 'rxjs';
+import {Item} from "./item";
 export interface Hero {
     id: number;
     name: string;
+    items: Item[];
   }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,17 @@
+
 import { Hero } from './hero';
+import { ITEMS } from "./mock-items";
+
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice', items: ITEMS },
+  { id: 12, name: 'Narco', items: ITEMS },
+  { id: 13, name: 'Bombasto', items: ITEMS },
+  { id: 14, name: 'Celeritas', items: ITEMS },
+  { id: 15, name: 'Magneta', items: ITEMS },
+  { id: 16, name: 'RubberMan', items: ITEMS },
+  { id: 17, name: 'Dynama', items: ITEMS },
+  { id: 18, name: 'Dr IQ', items: ITEMS },
+  { id: 19, name: 'Magma', items: ITEMS },
+  { id: 20, name: 'Tornado', items: ITEMS }
 ];
